df13a4a17be966f8fe7c33acbd9150d870b224c5
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-20 16:09:59 -0600

    Implemented simple example of socket.io usage

--- src/index.js
+++ src/index.js
@@ -13,8 +13,16 @@ const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-io.on('connection', () => {
+let count = 0;
+io.on('connection', socket => {
   console.log('New WebSocket connection');
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    io.emit('countUpdated', ++count);
+    console.log('incremented count to ' + count);
+  });
 });
 
 server.listen(port, () => {
