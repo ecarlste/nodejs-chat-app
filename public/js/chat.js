3f328075a72073d9e14097b80ef3b9d51344b81b
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-20 23:23:09 -0600

    Disable buttons until location/message receipt

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,28 +1,41 @@
 const socket = io();
 
+const $chatForm = document.querySelector('#chat-form');
+const $chatFormInput = $chatForm.querySelector('input');
+const $chatFormButton = $chatForm.querySelector('button');
+
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', text => {
   console.log(text);
 });
 
-document.querySelector('#chat-form').addEventListener('submit', event => {
+$chatForm.addEventListener('submit', event => {
   event.preventDefault();
+  $chatFormButton.setAttribute('disabled', 'disabled');
 
   const messageToSend = event.target.elements.message.value;
 
   socket.emit('sendMessage', messageToSend, () => {
     console.log('Message delivered!');
+    $chatFormButton.removeAttribute('disabled');
+    $chatFormInput.value = '';
+    $chatFormInput.focus();
   });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     alert('Your browser does not support geolocation');
   }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     socket.emit('sendLocation', { latitude, longitude }, () => {
       console.log('Location shared!');
+      $sendLocationButton.removeAttribute('disabled');
     });
   });
 });
